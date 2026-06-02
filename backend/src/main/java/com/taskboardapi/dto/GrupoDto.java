12f66a6c3085670edf7e506a679fe1de59ec1043
Patricia Lindoso <patricia.d2310@gmail.com>
2026-06-02 19:09:23 -0300

    feat: cria services para grupo e implementa dto

--- backend/src/main/java/com/taskboardapi/dto/GrupoDto.java
+++ backend/src/main/java/com/taskboardapi/dto/GrupoDto.java
@@ -1,4 +1,15 @@
 package com.taskboardapi.dto;
 
+import com.taskboardapi.models.Tarefa;
+import lombok.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+
 public class GrupoDto {
+    private Long id;
+    private String titulo;
+    private List<Tarefa> tarefas = new ArrayList<>();
 }

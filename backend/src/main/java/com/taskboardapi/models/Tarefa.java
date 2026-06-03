6ca8699f2c7d91ff36d6d973687931a4b4c5ec20
Patricia Lindoso <patricia.d2310@gmail.com>
2026-06-02 22:30:37 -0300

    fix: resolve loop infinito no retorno de grupos

--- backend/src/main/java/com/taskboardapi/models/Tarefa.java
+++ backend/src/main/java/com/taskboardapi/models/Tarefa.java
@@ -1,5 +1,6 @@
 package com.taskboardapi.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -18,6 +19,7 @@ public class Tarefa {
     private boolean completado = false;
     private LocalDate dataPrazo;
 
+    @JsonBackReference
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "grupo_id")
     private Grupo grupo;
